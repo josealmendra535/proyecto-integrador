9002c375c50b416132ea525639ba40bb988a343c
Jose Almendra <jalmendrav@gmail.com>
2024-10-04 00:01:40 -0300

    entrega1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import NavBar from './components/NavBar/NavBar'
 
 function App() {
 
   return (
     <div>
-      <h1>Hola mundo</h1>
+      <NavBar />
+
     </div>
   )
 }
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,60 @@
+
+import "./navbar.css"
+
+const NavBar = () => {
+    return (
+    <nav className="navbar">
+            <div className="brand">
+                <h2>Ecommerce</h2>
+            </div>
+            
+    </nav>
+        
+    ) 
+}
+export default NavBar
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
